cc183c033af5a447f0fbcd2374ae32b038c086a9
Dizze222 <lucia31@mail.ru>
2022-06-26 14:01:50 +0300

    added coroutines

--- app/src/main/java/p/pokemons/pokemonsapp/MainActivity.kt
+++ app/src/main/java/p/pokemons/pokemonsapp/MainActivity.kt
@@ -2,10 +2,14 @@ package p.pokemons.pokemonsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import p.pokemons.pokemonsapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
